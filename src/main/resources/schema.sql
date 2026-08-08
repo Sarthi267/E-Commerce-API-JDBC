f3f865e1fcf01d50dd60c3881f41bcf6e1400ab1
Sarthi267 <patel142006@gmail.com>
2026-08-08 16:54:14 -0400

    add indexes

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -70,6 +70,20 @@ create table if not exists "order_items"(
     updated_at timestamp not null default current_timestamp
 );;
 
+create index idx_user_username on users(username);
+create index idx_user_email on users(email);
+create index idx_listings_seller_id on listings(seller_id);
+create index idx_listings_product_id on listings(product_id);
+create index idx_orders_user_id on orders(user_id);
+create index idx_order_items_order_id on order_items(order_id);
+create index idx_order_items_listing_id on order_items(listing_id);
+
+create index idx_listings_status on listings(status);
+create index idx_orders_status on orders(status);
+create index idx_orders_payment_status on orders(payment_status);
+
+create index idx_listings_sku on listings(sku);
+
 create or replace function fn_update_updated_at()
        returns trigger as $$
        begin
